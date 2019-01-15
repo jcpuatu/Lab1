cbb66e2258460dd53f039b5c1b20ec5685ab4c38
JC Puatu <jcpuatu@gmail.com>
2019-01-15 14:51:52 +0800

    three files

---
+++ array-keys.php
@@ -0,0 +1,5 @@
+<?php
+
+$ceu = array("Italy"=>"Rome", "Luxemberg"=>"Luxemberg", "Belgium"=>"Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki"
+			, "France"=>"Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany"=>"Berlin", "Greece"=>"Athens"
+			, "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon,
